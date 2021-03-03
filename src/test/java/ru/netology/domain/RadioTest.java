4be392d7831805bfcf354b0be268fd3e7f78a8b1
Denis Zhigun <zhigundv@gmail.com>
2021-03-03 23:07:08 +0300

    Update RadioTest

--- src/test/java/ru/netology/domain/RadioTest.java
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -5,129 +5,129 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RadioTest {
-    Radio orange = new Radio();
+    Radio fruit = new Radio();
 
     @Test
     public void shouldNextStation() {
         int currentStation = 9;
-        orange.setCurrentStation(currentStation);
-        orange.nextRadioStation();
-        assertEquals(0, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.nextRadioStation();
+        assertEquals(0, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldNextStation1() {
         int currentStation = 5;
-        orange.setCurrentStation(currentStation);
-        orange.nextRadioStation();
-        assertEquals(6, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.nextRadioStation();
+        assertEquals(6, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldNextStation2() {
         int currentStation = 10;
-        orange.setCurrentStation(currentStation);
-        orange.nextRadioStation();
-        assertEquals(1, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.nextRadioStation();
+        assertEquals(1, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldPrevStation() {
         int currentStation = 0;
-        orange.setCurrentStation(currentStation);
-        orange.prevRadioStation();
-        assertEquals(9, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.prevRadioStation();
+        assertEquals(9, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldPrevStation1() {
         int currentStation = -1;
-        orange.setCurrentStation(currentStation);
-        orange.prevRadioStation();
-        assertEquals(9, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.prevRadioStation();
+        assertEquals(9, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldPrevStation2() {
         int currentStation = 5;
-        orange.setCurrentStation(currentStation);
-        orange.prevRadioStation();
-        assertEquals(4, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.prevRadioStation();
+        assertEquals(4, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldInputRadioStation() {
         int RadioStation = 7;
         int currentStation = 2;
-        orange.setCurrentStation(currentStation);
-        orange.inputRadioStation(RadioStation);
-        assertEquals(7, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.inputRadioStation(RadioStation);
+        assertEquals(7, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldInputRadioStation1() {
         int RadioStation = 10;
         int currentStation = 2;
-        orange.setCurrentStation(currentStation);
-        orange.inputRadioStation(RadioStation);
-        assertEquals(2, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.inputRadioStation(RadioStation);
+        assertEquals(2, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldInputRadioStation2() {
         int RadioStation = -1;
         int currentStation = 2;
-        orange.setCurrentStation(currentStation);
-        orange.inputRadioStation(RadioStation);
-        assertEquals(2, orange.getCurrentStation());
+        fruit.setCurrentStation(currentStation);
+        fruit.inputRadioStation(RadioStation);
+        assertEquals(2, fruit.getCurrentStation());
     }
 
     @Test
     public void shouldSoundVolumePlus() {
         int currentVolume = 10;
-        orange.setCurrentVolume(currentVolume);
-        orange.soundVolumePlus();
-        assertEquals(10, orange.getCurrentVolume());
+        fruit.setCurrentVolume(currentVolume);
+        fruit.soundVolumePlus();
+        assertEquals(10, fruit.getCurrentVolume());
     }
 
     @Test
     public void shouldSoundVolumePlus1() {
         int currentVolume = 5;
-        orange.setCurrentVolume(currentVolume);
-        orange.soundVolumePlus();
-        assertEquals(6, orange.getCurrentVolume());
+        fruit.setCurrentVolume(currentVolume);
+        fruit.soundVolumePlus();
+        assertEquals(6, fruit.getCurrentVolume());
     }
 
     @Test
     public void shouldSoundVolumePlus2() {
         int currentVolume = 11;
-        orange.setCurrentVolume(currentVolume);
-        orange.soundVolumePlus();
-        assertEquals(1, orange.getCurrentVolume());
+        fruit.setCurrentVolume(currentVolume);
+        fruit.soundVolumePlus();
+        assertEquals(1, fruit.getCurrentVolume());
     }
 
     @Test
     public void shouldSoundVolumeMinus() {
         int currentVolume = 5;
-        orange.setCurrentVolume(currentVolume);
-        orange.soundVolumeMinus();
-        assertEquals(4, orange.getCurrentVolume());
+        fruit.setCurrentVolume(currentVolume);
+        fruit.soundVolumeMinus();
+        assertEquals(4, fruit.getCurrentVolume());
     }
 
     @Test
     public void shouldSoundVolumeMinus1() {
         int currentVolume = 0;
-        orange.setCurrentVolume(currentVolume);
-        orange.soundVolumeMinus();
-        assertEquals(0, orange.getCurrentVolume());
+        fruit.setCurrentVolume(currentVolume);
+        fruit.soundVolumeMinus();
+        assertEquals(0, fruit.getCurrentVolume());
     }
 
     @Test
     public void shouldSoundVolumeMinus2() {
         int currentVolume = -1;
-            orange.setCurrentVolume(currentVolume);
-        orange.soundVolumeMinus();
-        assertEquals(0, orange.getCurrentVolume());
+        fruit.setCurrentVolume(currentVolume);
+        fruit.soundVolumeMinus();
+        assertEquals(0, fruit.getCurrentVolume());
     }
 
 
